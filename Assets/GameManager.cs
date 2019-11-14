935ed5c0523de2f08d6028731b572809b4fd5f9e
Dahye Kim <whdhkdy16@gmail.com>
2019-11-14 10:40:55 +0900

    설정창 On/Off

---
+++ Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ob;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ob.SetActive(false);
+    }
+
+    public void ObjOn()
+    {
+        ob.SetActive(true);
+    }
+
+    public void ObjOff()
+    {
+        ob.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
